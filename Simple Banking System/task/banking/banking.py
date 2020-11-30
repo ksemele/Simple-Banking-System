3fe6661c3c49590c5703ab885a974de9c7d5a87e
ksemele <greengrunt@gmail.com>
2020-12-01 00:30:44 +0300

    second day of coding that
    a little planning to generate card_number, PIN && storage

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -7,19 +7,36 @@ class Menu:
 	def __init__(self):
 		pass
 
-	def start_menu(self):
-		# print('1. Create an account\n' + '2. Log into account\n' + '0. Exit')
+	def print(self):
+		for each in self.elems:
+			print(each)
+
+
+class MenuLogin:
+	elems = ['1. Balance', '2. Log out', '0. Exit']
+		# ['Enter your card number:', 'Enter your PIN:', 'Wrong card number or PIN!', 'You have successfully logged in!']
+
+	def __init__(self):
+		pass
+
+	def print(self):
 		for each in self.elems:
 			print(each)
 
 
 class Card:
-	number = 4000000000000000
-	pin = ""
+	number = '400000'  # 0000000000'  # 400000
+	pin = ''
+	balance = 0
 
 	def __init__(self):
-		number = rand_card_number()
+		self.rand_card_number()
 		self.rand_pin()
+
+	def rand_card_number(self):
+		for i in range(1, 10):
+			i = randint(0, 9)
+			self.number += str(i)
 
 	def rand_pin(self):
 		for i in range(0, 4):
@@ -30,26 +47,41 @@ class Card:
 		return 'Your card number:\n{}\nYour card PIN:\n{}'.format(self.number, self.pin)
 
 
-def rand_card_number():
-	return 34
+class Storage:  # todo find_card() in storage
+	cards = []
+	cards_counter = 0
+
+	def __init__(self):
+		pass
+
+	def add_card(self, new_card):  # todo add only if !found same number in storage
+		self.cards.append(new_card)
 
 
-def choice_treat(choice):
-	if choice == 0:
+def create_card():
+	new_card = Card()  # todo simplify to 1 return
+	print(new_card)
+	return new_card
+
+
+def choice_treat(choice, storage):
+	if choice == 0:  # exit
 		print('_0')
-	elif choice == 1:
-		print('_1')
-	elif choice == 2:
-		print('_2')
+	elif choice == 1:  # Create account
+		# print('_1')
+		storage.add_card(create_card())
+		storage.cards_counter += 1
+	elif choice == 2:  # login
+		menu_login.print()
 	else:
 		print('Wrong choice')
 
 
+storage = Storage()
 menu = Menu()
-menu.start_menu()
-choice = int(input())
-choice_treat(choice)
+menu_login = MenuLogin()
 
-card = Card()
-print(card)
-# print(card.pin)
+menu.print()
+print(storage.cards)
+choice = int(input())
+choice_treat(choice, storage)
